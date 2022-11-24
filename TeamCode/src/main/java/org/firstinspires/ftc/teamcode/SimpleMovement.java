c591a7dbea51d1fb936ad125caf2d8ff054add16
Zachinquarantine <Zachinquarantine@protonmail.com>
2022-11-24 16:00:37 -0500

    Disable all the unused opmodes
    (if autonomous breaks, revert the changes to AprilTagAutonomous.java)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleMovement.java
@@ -30,6 +30,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -50,6 +51,7 @@ import com.qualcomm.robotcore.util.Range;
  */
 
 @TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
+@Disabled
 public class SimpleMovement extends LinearOpMode {
 
     // Declare OpMode members.
